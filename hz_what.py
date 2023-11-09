b1f035b5c22e73936eaf84d66b46f34c49c55731
unknown <Zabivaka95@gmail.com>
2023-11-09 13:10:17 +0600

    v10.23 psycopg2 was added

--- hz_what.py
+++ hz_what.py
@@ -1,15 +1,38 @@
-class Person: 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age 
+# class Person: 
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age 
 
-    def introduce(self):
-        return f"Hello, my name is {self.name} and i am {self.age} years old"
+#     def introduce(self):
+#         return f"Hello, my name is {self.name} and i am {self.age} years old"
     
-Person1 = Person("Alice",30)
+# Person1 = Person("Alice",30)
 
-print("name:", Person1.name)
-print("age:", Person1.age)
+# print("name:", Person1.name)
+# print("age:", Person1.age)
 
-print(Person1.introduce())
+# print(Person1.introduce())
 #afasfasfasfasf VERSION
+
+import psycopg2
+
+conn = psycopg2.connect(host = '127.0.0.1',
+                        database = 'sololeveling',
+                        user = 'postgres',
+                        password = '1234567')
+
+cur = conn.cursor()
+cur.execute('Select company_name from production_company limit 5;')
+username = [r[0] for r in cur.fetchall()]
+Found= False
+while not Found:
+    username = input('Введите свой логин: ')
+    if username in usernames:
+        print('Поздравляю вы в списке')
+        Found=True
+    else:
+        print('Вы не местный. Зарегестрируйтесь правельно)')
+
+conn.commit()
+cur.close()
+conn.close()
